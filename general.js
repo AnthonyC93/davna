c8bc7f999a1df07748de735feeced4e84cb18c3c
acarta <anthonym.cartagena@gmail.com>
2020-09-27 23:58:22 -0400

    feat: added media queries to make website desktop friendly

--- general.js
+++ general.js
@@ -3,58 +3,78 @@
 // // Then we set the value in the --vh custom property to the root of the document
 // document.documentElement.style.setProperty("--vh", `${vh}px`);
 // console.log("current vh is: ", vh);
+
+const fallback = ["K5gE2m1Svtw", "nu5qOYcrF4I", "2zhhollb_ec", "RWli6jI3U5s"];
 const loader = document.getElementById("loader");
 const videoSection = document.getElementById("videos");
-const loaderHtml = "<img id='loader' src='./assets/icons/loader.svg' />";
 
 function clearVideoContainer() {
   videoSection.innerHTML = "";
 }
-function showLoader() {
-  clearVideoContainer();
-  videoSection.innerHTML = loaderHtml;
-}
-
-function hideLoader() {
-  loader.style.display = "none";
-}
 
 function getVideos() {
-  showLoader();
-  console.log("getting vids");
-  let url =
-    "https://www.googleapis.com/youtube/v3/search?order=date&part=snippet&channelId=UCgzt1OTiHXn7R75zRsAHdLg&maxResults=25&key=AIzaSyBYsepI94PY97zc30fuDdUJ4p-qFUE2oSw&type=video";
+  const limit = 25;
+  const playlistId = "UUgzt1OTiHXn7R75zRsAHdLg";
+  const key = "AIzaSyBYsepI94PY97zc30fuDdUJ4p-qFUE2oSw";
+  const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails&part=snippet&playlistId=${playlistId}&key=${key}&maxResults=${limit}`;
   fetch(url)
     .then((data) => {
-      return data.json();
+      if (data.ok) {
+        console.log("data is ok", data.ok);
+        let videos = data.json().items;
+        displayVideos(videos);
+      } else {
+        console.log("data is NOT ok", data.ok);
+        throw data;
+      }
     })
-    .then(displayVideos)
     .catch((error) => {
       console.error(error);
+      useFallbacks();
     });
 }
 
-function displayVideos(data) {
-  let videos = data.items;
-  let container = document.getElementById("videos");
+function displayVideos(videos) {
   clearVideoContainer();
-
+  addVideo(fallback[0]);
   for (let i = 0; i < videos.length; i++) {
-    let videoContainer = document.createElement("div");
-    let id = videos[i]["id"]["videoId"];
-    videoContainer.innerHTML = `<div class="video-container fade mb">
-        <iframe
-          width="853"
-          height="480"
-          src="https://www.youtube.com/embed/${id}?autoplay=0&controls=1"
-          frameborder="0"
-          allowfullscreen
-        >
-        </iframe>
-      </div>`;
-    container.appendChild(videoContainer);
+    let id = videos[i]["contentDetails"]["videoId"];
+    if (videos.length < 3) {
+      addVideo(id, `e${videos.length}`);
+    } else {
+      addVideo(id);
+    }
   }
-  container.appendChild(loaderHtml);
+}
+
+function useFallbacks() {
+  clearVideoContainer();
+  for (let i = 0; i < fallback.length; i++) {
+    let id = fallback[i];
+    if (fallback.length < 3) {
+      addVideo(id, `e${fallback.length}`);
+    } else {
+      addVideo(id);
+    }
+  }
+}
+
+function addVideo(id, addClass) {
+  let videoContainer = document.createElement("div");
+  let classesToAdd = "video-container fade mb";
+  if (addClass && typeof addClass == "string") {
+    classesToAdd += " " + addClass;
+  }
+  videoContainer.setAttribute("class", classesToAdd);
+  videoContainer.innerHTML = `<iframe
+                                    width="853"
+                                    height="480"
+                                    src="https://www.youtube.com/embed/${id}?autoplay=0&controls=1"
+                                    frameborder="0"
+                                    allowfullscreen
+                                >
+                                </iframe>`;
+  videoSection.appendChild(videoContainer);
 }
 
 getVideos();
